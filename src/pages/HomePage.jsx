3955605601543d81f3e51dc0129830353de22026
Felipe França <felipe.fvieira@telefonica.com>
2025-01-12 22:23:29 -0300

    feat: added Home page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import NavBar from '../components/macro-components/navbar';
+const HomePage = () => {
+    return (
+        <div>
+            <NavBar></NavBar>
+        </div>
+    )
+}
+
+export default HomePage
