e18c93032397dacda9fbf766c75e1b259b9a66d8
Felipe França <felipe.fvieira@telefonica.com>
2025-01-12 22:25:38 -0300

    fix

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,7 +3,7 @@ import axios  from "axios";
 const baseURL = 'https://api.github.com';
 const baseUrlRepos = `${baseURL}/repos/FelipeFVieira/pulumi-iac`;
 
-const token = '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'
+const token = ''
 
 const githubApiRepos = axios.create({
   baseURL: baseUrlRepos,
